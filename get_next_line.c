dfde16d301dcbc5a7f8957ea6a7c3c439d7679ac
qmuntada <quentin.muntadas@orange.fr>
2015-03-17 14:32:14 +0100

    maj

--- get_next_line.c
+++ get_next_line.c
@@ -12,45 +12,73 @@
 
 #include "get_next_line.h"
 
-int		check_buf(char *buf, int pivot, char **line)
+int		line_verif(char **line, char **tmp, int res, char **str)
 {
-	int				start;
-	char			*str;
-
-	start = (pivot -= 1) + 1;
-	while (++pivot < BUFF_SIZE)
-		if (buf[pivot] == '\n')
-		{
-			str = ft_memalloc(ft_strlen(*line) + ft_strlen(buf));
-			str = ft_strcpy(str, *line);
-			str = ft_strncat(str, buf, ++pivot);
-			*line = str;
-			return (pivot);
-		}
-	str = ft_memalloc(ft_strlen(*line) + ft_strlen(buf + start));
-	str = ft_strcpy(str, *line);
-	str = ft_strncat(str, buf + start, pivot - start);
-	*line = str;
-	return (-1);
+	if (res == 0 && ft_strlen(*tmp) > 0)
+	{
+		*line = *tmp;
+		*str = NULL;
+		*tmp = NULL;
+		return (1);
+	}
+	if (res == 0 && ft_strlen(*tmp) == 0)
+		return (0);
+	return (res);
 }
 
-int		get_next_line(int fd, char **line)
+char	*read_line(char *tmp)
 {
-	static char		buf[BUFF_SIZE + 1];
-	static int		pivot = 0;
-	int				status;
+	size_t			t;
+	char			*line;
 
-	if (fd < 0 || !line || BUFF_SIZE > 100000)
+	t = 0;
+	while (tmp[t] != '\n')
+		t++;
+	line = (char *)malloc((t + 1) * sizeof(char));
+	line = ft_strncpy(line, tmp, t);
+	line[t] = '\0';
+	return (line);
+}
+
+char	*cpycat(char *s1, char *s2)
+{
+	char *tmp;
+
+	tmp = NULL;
+	if ((s1 && !s2) || (!s1 && s2))
+		return (s1 ? s1 : s2);
+	if (!s1 && !s2)
+		return (NULL);
+	tmp = ft_memalloc(ft_strlen(s1) + ft_strlen(s2));
+	tmp = ft_strcpy(tmp, s1);
+	tmp = ft_strncat(tmp, s2, ft_strlen(s2));
+	return (tmp);
+}
+
+int					get_next_line(int const fd, char **line)
+{
+	static char				*str;
+	int						res;
+	char					*buf;
+	char					*tmp;
+
+	if (fd < 0 || !line || BUFF_SIZE < 1 || BUFF_SIZE > 10000000)
 		return (-1);
-	*line = ft_strnew(1);
-	pivot = check_buf(buf, pivot, line);
-	if (pivot >= 0)
-		return (1);
-	while ((status = read(fd, buf, BUFF_SIZE)) > 0)
+	buf = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1));
+	if (str == NULL)
+		str = ft_strnew(BUFF_SIZE);
+	tmp = ft_strncpy(ft_memalloc(BUFF_SIZE), str, BUFF_SIZE);
+	while (!(ft_strchr(tmp, '\n')))
 	{
-		pivot = check_buf(buf, 0, line);
-		if (pivot >= 0)
-			return (1);
+		if ((res = read(fd, buf, BUFF_SIZE)) < 1)
+			return (line_verif(line, &tmp, res, &str));
+		buf[res] = '\0';
+		tmp = cpycat(tmp, buf);
 	}
-	return (status);
+	*line = read_line(tmp);
+	if (ft_strchr(tmp, '\n'))
+		str = ft_strncpy(str, ft_strchr(tmp, '\n') + 1, BUFF_SIZE);
+	free(tmp);
+	free(buf);
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -19,7 +19,7 @@
 # include <stdlib.h>
 # include "libft/libft.h"
 
-# define BUFF_SIZE 80000
+# define BUFF_SIZE 256
 
 int		get_next_line(int const fd, char **line);
 
